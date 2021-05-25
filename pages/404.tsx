3cfbe86981ec83f8e69dc6767bb9d0f9b3de18cf
TrafalMar <vladiksav2@gmail.com>
2021-05-25 21:51:09 +0300

    first commit

---
+++ pages/404.tsx
@@ -0,0 +1,95 @@
+export default function NotFound() {
+  return (
+    <main className="container">
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">4</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <span className="particle">0</span>
+      <article className="content">
+        <p>Damnit stranger,</p>
+        <p>
+          You got lost in the <strong>404</strong> galaxy.
+        </p>
+        <p>
+          <button>Go back to earth.</button>
+        </p>
+      </article>
+    </main>
+  );
+}
